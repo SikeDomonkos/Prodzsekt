05df57b9dd57f87d03a07abc290ad37b150abd14
JanosiMarcell <janosim@kkszki.hu>
2025-02-27 07:59:57 +0100

    bolondos egyszerű bejelentkezés

---
+++ ProjektWPF/LoginWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjektWPF
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private bool isPasswordVisible = false;
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void TogglePasswordVisibility(object sender, RoutedEventArgs e)
+        {
+            isPasswordVisible = !isPasswordVisible;
+            MessageBox.Show("Password visibility toggled."); // Implement actual toggle logic
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameBox.Text;
+            string password = PasswordBox.Password;
+
+            // Egyszerű bejelentkezési ellenőrzés (teszt értékek)
+            if (username == "admin" && password == "1234")
+            {
+                
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Hibás felhasználónév vagy jelszó!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
