c87a17dd8322d490c6d29dac2ff1cb245c681585
KovacsSzabolcs <90681643+Kszaboli@users.noreply.github.com>
2025-03-07 09:48:11 +0100

    user table updated
    (default date of birth now 2000-01-01 00:00:00)

--- auth_withdata.sql
+++ auth_withdata.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Feb 19. 17:00
+-- Létrehozás ideje: 2025. Már 07. 09:45
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -96,7 +96,7 @@ CREATE TABLE `aspnetusers` (
   `Id` varchar(255) NOT NULL,
   `FullName` longtext NOT NULL,
   `Age` int(11) NOT NULL,
-  `DateOfBirth` date NOT NULL,
+  `DateOfBirth` datetime NOT NULL DEFAULT '2000-01-01 00:00:00',
   `UserName` varchar(256) DEFAULT NULL,
   `NormalizedUserName` varchar(256) DEFAULT NULL,
   `Email` varchar(256) DEFAULT NULL,
@@ -121,8 +121,9 @@ CREATE TABLE `aspnetusers` (
 --
 
 INSERT INTO `aspnetusers` (`Id`, `FullName`, `Age`, `DateOfBirth`, `UserName`, `NormalizedUserName`, `Email`, `NormalizedEmail`, `EmailConfirmed`, `PasswordHash`, `SecurityStamp`, `ConcurrencyStamp`, `PhoneNumber`, `PhoneNumberConfirmed`, `TwoFactorEnabled`, `LockoutEnd`, `LockoutEnabled`, `AccessFailedCount`, `Fizetesi_elmaradas`, `FizetettE_havi`, `LakasSzovNev`) VALUES
-('48288b35-9820-4015-affc-387f80e4ee7c', 'Gannon Mattedi', 0, '2001-09-11', 'gmattedi0', 'GMATTEDI0', 'gmattedi0@wsj.com', 'GMATTEDI0@WSJ.COM', 0, 'AQAAAAIAAYagAAAAELd5QWNdJJttcvolWoeKT7YI+lc3y01j7lMXjbvQK4E7Ctj39XyJc7a2eTdEzKsTZw==', 'XYWUE74EOBK7GPX7PTKWVAOBMFYSLJT6', 'e93a5ae2-34a6-40ec-978b-381188b61616', NULL, 0, 0, NULL, 1, 0, 0, 0, ''),
-('e0c37b9c-543c-428f-bd6e-d582e7f590d6', 'Próba János', 0, '1986-11-15', 'PJanos86', 'PJANOS86', 'Pjanos1986@gmail.com', 'PJANOS1986@GMAIL.COM', 0, 'AQAAAAIAAYagAAAAEDLuGhUZpuiO7Rr2v1w0rEzhcCf3HbD/z7DKkk32NpnUNg50ddoL4v6yRJIMUfvVUQ==', 'JXF5N4VEDVCEO3HDD5Q7KXAWOHJVLJ4N', 'bfe69a23-11c4-48d5-9147-5c3aedf29e8a', NULL, 0, 0, NULL, 1, 0, 0, 0, '');
+('48288b35-9820-4015-affc-387f80e4ee7c', 'Gannon Mattedi', 0, '2025-03-07 08:32:36', 'gmattedi0', 'GMATTEDI0', 'gmattedi0@wsj.com', 'GMATTEDI0@WSJ.COM', 0, 'AQAAAAIAAYagAAAAELd5QWNdJJttcvolWoeKT7YI+lc3y01j7lMXjbvQK4E7Ctj39XyJc7a2eTdEzKsTZw==', 'XYWUE74EOBK7GPX7PTKWVAOBMFYSLJT6', 'e93a5ae2-34a6-40ec-978b-381188b61616', 'string', 0, 0, NULL, 1, 0, 0, 0, 'string'),
+('e0c37b9c-543c-428f-bd6e-d582e7f590d6', 'Próba János', 0, '2000-01-01 00:00:00', 'PJanos86', 'PJANOS86', 'Pjanos1986@gmail.com', 'PJANOS1986@GMAIL.COM', 0, 'AQAAAAIAAYagAAAAEDLuGhUZpuiO7Rr2v1w0rEzhcCf3HbD/z7DKkk32NpnUNg50ddoL4v6yRJIMUfvVUQ==', 'JXF5N4VEDVCEO3HDD5Q7KXAWOHJVLJ4N', 'bfe69a23-11c4-48d5-9147-5c3aedf29e8a', NULL, 0, 0, NULL, 1, 0, 0, 0, ''),
+('e15197b7-d32b-4090-8c88-fcaece125f20', 'string', 0, '2025-03-11 00:00:00', 'ujregister', 'UJREGISTER', 'string', 'STRING', 0, 'AQAAAAIAAYagAAAAEOyUYgxd3uz9hLAdKWD7nOD9Mh7utNvc1p/dGeOyZNYFDLwL5JAhSEcr1kzb9fJwyA==', 'RQIFLGD3IT4KAXHLPI25PWYNEY2NJIF6', 'e880e025-d694-484e-b762-42516947de20', 'string', 0, 0, NULL, 1, 0, 0, 0, 'string');
 
 -- --------------------------------------------------------
 
@@ -188,6 +189,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`Id`, `Title`, `Description`, `Poster_id`, `Created_at`, `Updated_at`, `Is_accepted`, `Acceptor_id`, `Location`) VALUES
+('181bcbf7-ba6c-47b3-ad16-0e8fb86da462', 'string', 'string', '48288b35-9820-4015-affc-387f80e4ee7c', '2025-02-21 12:02:35', '0001-01-01 00:00:00', 0, NULL, 'string'),
 ('2cad4a40-3c8b-4344-b8ed-b73b2e94c5ae', 'Roofing (Metal)', 'morbi vel lectus in quam fringilla rhoncus mauris enim leo rhoncus sed vestibulum sit amet cursus id turpis integer', 'c6566929-3913-484f-b57e-1a00c3758e14', '2023-06-03 19:02:32', '2022-10-10 07:40:02', 0, '8952a9f4-afa9-4ddb-8e0f-03500fc788f4', '9 Southridge Road'),
 ('49e1d27f-078f-4aeb-a4d2-1f6d3521e090', 'Construction Clean and Final Clean', 'rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis nam', 'a342cef4-8178-4e83-bdac-619fc06e22ca', '2022-01-07 09:12:00', '2024-09-07 03:02:40', 0, '9d2b5beb-d1e2-4df2-8e14-f00722aeac2d', '336 Roxbury Park'),
 ('4a35776c-7cdb-4a45-bd3d-1681513856d8', 'Site Furnishings', 'sagittis nam congue risus semper porta volutpat quam pede lobortis ligula sit amet eleifend pede libero quis orci nullam molestie nibh in lectus pellentesque at', '75a1560b-c6a7-4f29-aa28-4318e0de6b1c', '2020-06-11 23:23:02', '2021-06-18 08:58:03', 0, '1cefccfa-e482-4a03-b326-0ac95ba745d6', '70561 Quincy Avenue'),
