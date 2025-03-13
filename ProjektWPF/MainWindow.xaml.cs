489b5df0ed20b5d4e0b0a78428ddf80ccec47289
JanosiMarcell <janosim@kkszki.hu>
2025-03-13 10:01:10 +0100

    Kijelentkezés fele kész

--- ProjektWPF/MainWindow.xaml.cs
+++ ProjektWPF/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -25,6 +26,37 @@ namespace ProjektWPF
         {
             InitializeComponent();
         }
+        private bool isSidebarOpen = false;
+
+        private void ToggleSidebar_Click(object sender, RoutedEventArgs e)
+        {
+            double targetX = isSidebarOpen ? 200 : 0;
+            isSidebarOpen = !isSidebarOpen;
+
+            var animation = new DoubleAnimation
+            {
+                To = targetX,
+                Duration = TimeSpan.FromSeconds(0.3),
+                EasingFunction = new QuadraticEase()
+            };
+
+            SidebarTransform.BeginAnimation(TranslateTransform.XProperty, animation);
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window != this) // Csak ha nem ez az ablak
+                {
+                    window.Close();
+                }
+            }
+
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+            this.Close();
+        }
 
         private void OpenDataGridWindow_Click(object sender, RoutedEventArgs e)
         {
@@ -37,6 +69,7 @@ namespace ProjektWPF
             SzavazasLekeres szavazasLekeres = new SzavazasLekeres();
             szavazasLekeres.Show();
         }
+
 
     }
 }
